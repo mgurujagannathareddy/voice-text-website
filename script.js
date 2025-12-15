8ef1803556a7e097a807505b42f69cc2246c87d6
mgurujagannathareddy <m.gurujagannathareddy@gmail.com>
2025-12-15 19:12:03 +0530

    script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const SpeechRecognition =
 
 const recognition = new SpeechRecognition();
 
-recognition.continuous = true;      
-recognition.interimResults = false; 
+recognition.continuous = true;
+recognition.interimResults = false;
 
 let isUserStopped = false;
 let isListening = false;
@@ -12,14 +12,13 @@ let isListening = false;
 function startListening() {
     if (isListening) return;
 
-    isUserStopped = false;
     isListening = true;
+    isUserStopped = false;
 
     document.getElementById("output").value = "";
-    const selectedLanguage = document.getElementById("language").value;
-    recognition.lang = selectedLanguage;
-
     document.getElementById("status").innerText = "Status: Listening...";
+
+    recognition.lang = document.getElementById("language").value;
     recognition.start();
 }
 
@@ -34,7 +33,6 @@ function stopListening() {
 recognition.onresult = function (event) {
     const index = event.resultIndex;
     const transcript = event.results[index][0].transcript;
-
     document.getElementById("output").value += transcript + " ";
 };
 
